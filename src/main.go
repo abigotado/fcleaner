735195b1d487e224da2689008a623490a3b3c01d
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-06-05 06:09:33 -0300

    gitignire

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -22,35 +24,53 @@ func runCmd(name string, args []string, workDir string) error {
 	return nil
 }
 
-func main() {
+// doAll последовательно запускает все необходимые команды
+func doAll(useFvm bool) error {
+	flutterArgs := func(args ...string) (string, []string) {
+		if useFvm {
+			return "fvm", append([]string{"flutter"}, args...)
+		}
+		return "flutter", args
+	}
+
 	// Шаг 1: flutter clean
-	if err := runCmd("flutter", []string{"clean"}, ""); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	cmd, args := flutterArgs("clean")
+	if err := runCmd(cmd, args, ""); err != nil {
+		return fmt.Errorf("ошибка при запуске flutter clean: %w", err)
 	}
 
 	// Шаг 2: flutter pub get
-	if err := runCmd("flutter", []string{"pub", "get"}, ""); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	cmd, args = flutterArgs("pub", "get")
+	if err := runCmd(cmd, args, ""); err != nil {
+		return fmt.Errorf("ошибка при запуске flutter pub get: %w", err)
 	}
 
-	// Рабочая директория для iOS
 	iosDir := "ios"
 
-	// Шаг 3: Удаление Podfile.lock и папки Pods в директории ios
+	// Шаг 3.1: Удаляем Podfile.lock
 	if err := runCmd("rm", []string{"-rf", "Podfile.lock"}, iosDir); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return fmt.Errorf("ошибка при удалении Podfile.lock: %w", err)
 	}
+
+	// Шаг 3.2: Удаляем Pods
 	if err := runCmd("rm", []string{"-rf", "Pods"}, iosDir); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return fmt.Errorf("ошибка при удалении Pods: %w", err)
 	}
 
 	// Шаг 4: pod install --repo-update
 	if err := runCmd("pod", []string{"install", "--repo-update"}, iosDir); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return fmt.Errorf("ошибка при запуске pod install --repo-update: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	useFvm := flag.Bool("fvm", false, "Use FVM to run flutter commands (fvm flutter ...)")
+	flag.Parse()
+
+	if err := doAll(*useFvm); err != nil {
+		// Используем log.Fatal, чтобы вывести ошибку и завершить программу
+		log.Fatal(err)
 	}
 }
